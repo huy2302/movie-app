8cc764ef5827ce4d6eb5301f783e09793da5d707
XuanThuy17 <92718177+XuanThuy17@users.noreply.github.com>
2023-11-11 03:06:58 +0700

    register
    register

--- app/src/main/java/com/example/moviefilmapp/Activities/RegisterActivity.java
+++ app/src/main/java/com/example/moviefilmapp/Activities/RegisterActivity.java
@@ -1,16 +1,15 @@
 package com.example.moviefilmapp.Activities;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.moviefilmapp.R;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -18,40 +17,27 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class LoginActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
     private EditText userEdt, passEdt;
-    private Button loginBtn;
+    private Button registerBtn;
     private FirebaseAuth mAuth;
-    private TextView registerText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
         mAuth = FirebaseAuth.getInstance();
 
         userEdt = findViewById(R.id.editTextText);
         passEdt = findViewById(R.id.editTextPassword);
-        loginBtn = findViewById(R.id.loginBtn);
-        registerText = findViewById(R.id.textRegister);
+        registerBtn = findViewById(R.id.registerBtn);
 
-        loginBtn.setOnClickListener(new View.OnClickListener() {
+        registerBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {login();}
-        });
-        registerText.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-               register();
-            }
+            public void onClick(View v) {register();}
         });
     }
 
     private void register() {
-        Intent i = new Intent(LoginActivity.this,RegisterActivity.class);
-        startActivity(i);
-    }
-
-    private void login() {
         String email,password;
         email = userEdt.getText().toString();
         password = passEdt.getText().toString();
@@ -63,15 +49,15 @@ public class LoginActivity extends AppCompatActivity {
             Toast.makeText(this,"Vui long nhap mat khau!",Toast.LENGTH_LONG).show();
             return;
         }
-        mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+        mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if(task.isSuccessful()){
-                    Toast.makeText(getApplicationContext(),"Dang nhap thanh cong!",Toast.LENGTH_LONG).show();
-                    Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                    Toast.makeText(getApplicationContext(),"tao tai khoan thanh cong", Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(RegisterActivity.this,MainActivity.class);
                     startActivity(intent);
                 }else {
-                    Toast.makeText(getApplicationContext(),"Dang nhap khong thanh cong", Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(), "tao tai khoan khong thanh cong",Toast.LENGTH_LONG).show();
                 }
             }
         });
